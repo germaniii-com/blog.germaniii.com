9e6f221be79fde7ae0bbaf8a2ad15e96195cdc80
German III <germaniiifelisarta@gmail.com>
2023-11-08 19:58:44 +0800

    Add button styles similar to germaniii.com

---
+++ gatsby/src/components/NavbarButton.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const NavbarButton = ({ name }) => {
+  const pathname = window.location.pathname;
+  const currentPageStyle = "bg-primary text-white";
+  const normalButtonStyle = "bg-white text-primary hover:text-primary-hover";
+  const isCurrentPage =
+    (pathname === "/" && name.toLowerCase() === "home") ||
+    pathname.toLowerCase() === name.toLowerCase();
+
+  return (
+    <>
+      <li
+        class={`pointer block p-3 rounded ${
+          isCurrentPage ? currentPageStyle : normalButtonStyle
+        }`}
+      >
+        {name}
+      </li>
+    </>
+  );
+};
+
+export default NavbarButton;
