572156d65b1a7353dd46781a6b2cca8809f2e971
German III <germaniiifelisarta@gmail.com>
2023-11-11 12:09:55 +0800

    Redesign home

--- gatsby/src/components/home/FeaturedArticleSection.jsx
+++ gatsby/src/components/home/FeaturedArticleSection.jsx
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 
 const FeaturedArticleSection = () => {
   return (
-    <div class="self-center text-primary rounded-xl p-5 flex gap-y-5 flex-col-reverse md:flex-row md:gap-x-5">
+    <div class="self-center bg-primary text-white rounded-xl p-5 flex gap-y-5 flex-col-reverse md:flex-row md:gap-x-5">
       <div class="grid grid-rows-[1fr_auto_auto_auto] gap-y-1">
         <span class="text-md">Featured Articles</span>
         <span class="text-2xl font-bold">{"Beginner's Guide to Linux"}</span>
@@ -12,7 +12,7 @@ const FeaturedArticleSection = () => {
             .slice(0, 100)
             .concat("...")}
         </p>
-        <div class="w-fit h-fit rounded-full bg-primary text-white p-1.5 text-xs font-bold">
+        <div class="w-fit h-fit rounded-full bg-accent text-white p-1.5 text-xs font-bold">
           <Link to="/">{"Read More"}</Link>
         </div>
       </div>
