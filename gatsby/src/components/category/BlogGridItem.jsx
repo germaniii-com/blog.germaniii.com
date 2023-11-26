18e9059b118e95d39a8061ec0ebbc5f09c8bdcb4
German III <germaniiifelisarta@gmail.com>
2023-11-26 18:50:07 +0800

    Update Date Time format in BlogGridItem

--- gatsby/src/components/category/BlogGridItem.jsx
+++ gatsby/src/components/category/BlogGridItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { strapiURL } from "../../constants";
 import { Link } from "gatsby";
+import dayjs from "dayjs";
 
 const BlogGridItem = ({ id, title, date, cover, tags }) => {
   return (
@@ -8,7 +9,9 @@ const BlogGridItem = ({ id, title, date, cover, tags }) => {
       <img src={`${strapiURL}${cover}`} alt="coverphoto" />
       <Link className="cursor-pointer" to={`/post/${id}`}>
         <div className="text-primary text-base font-bold">{title}</div>
-        <div className="text-primary text-xs">{date}</div>
+        <div className="text-primary text-xs">
+          {dayjs(date).format("MM-DD-YYYY HH:mm").toString()}
+        </div>
         <div className="text-primary text-xs">
           {tags.map((tag, index) =>
             index === tags.length - 1 ? ` ${tag}` : ` ${tag} |`
