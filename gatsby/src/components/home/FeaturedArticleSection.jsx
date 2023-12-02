a7ef3466094a00348afc87e25a6536cea7bf9852
German III <germaniiifelisarta@gmail.com>
2023-12-02 11:25:02 +0800

    Update Featured Article Layout

--- gatsby/src/components/home/FeaturedArticleSection.jsx
+++ gatsby/src/components/home/FeaturedArticleSection.jsx
@@ -5,7 +5,7 @@ import { strapiURL } from "../../constants";
 const FeaturedArticleSection = ({ post }) => {
   return (
     <div className="self-center bg-primary text-white rounded-xl p-5 flex gap-y-5 flex-col-reverse md:flex-row md:gap-x-5">
-      <div className="grid grid-rows-[1fr_auto_auto_auto] gap-y-1">
+      <div className="grid grid-rows-[auto_auto_1fr_auto] gap-y-1">
         <span className="text-md">Featured Articles</span>
         <span className="text-2xl font-bold">{post.title}</span>
         <p className="text-s pb-3 md:pb-1">
