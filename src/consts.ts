fc7fd84dfd42b20d1c8db248d47156b1ab5c9de5
German III <germaniiifelisarta@gmail.com>
2024-05-02 15:35:51 +0800

    Update website title

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = "wiki • German III";
+export const SITE_TITLE = "Wiki • German III";
 export const SITE_DESCRIPTION =
   "This is a wiki of German III containing all learnings and experiences for future reference.";
