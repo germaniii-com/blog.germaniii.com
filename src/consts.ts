95f8d268226afec92984932a8c7fadffc365e0f5
German III Felisarta <germaniiifelisarta@gmail.com>
2024-09-03 00:12:58 +0800

    [Refactor] organization of styles and css

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,21 @@
 export const SITE_TITLE = "Wiki • German III";
 export const SITE_DESCRIPTION =
   "This is a wiki of German III containing all learnings and experiences for future reference.";
+
+export const socialLinks = [
+  {
+    link: "https://www.linkedin.com/in/german-iii-felisarta-648b9420b",
+    description: "Follow Me on LinkedIn",
+    image: "icons/linkedin.svg",
+  },
+  {
+    link: "https://gitlab.com/germaniiifelisarta",
+    description: "My Gitlab repo",
+    image: "icons/gitlab.svg",
+  },
+  {
+    link: "https://github.com/germaniii",
+    description: "My Github repo",
+    image: "icons/github.svg",
+  },
+];
